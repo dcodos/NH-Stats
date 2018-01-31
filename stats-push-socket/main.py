ea3c71571a5ebc1bc9a655a6a8e7d18ff4f9d51c
Daniel Codos <dbc5ba@virginia.edu>
2018-01-31 18:01:49 -0500

    Num of devices

--- stats-push-socket/main.py
+++ stats-push-socket/main.py
@@ -1,7 +1,13 @@
 import requests
+import json
 
 base_url = 'http://localhost:38080/api?command='
 
 command_info = '{"id":1,"method":"device.list","params":[]}'
 res = requests.get(base_url + command_info)
-print(res.content)
+
+dev_info = json.loads(res.content)["devices"]
+num_devices = len(dev_info)
+
+print(dev_info)
+print(num_devices)
