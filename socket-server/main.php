e7b8e1fa8293753555f84081c9cbe9eb0bcc5368
Daniel Codos <dbc5ba@virginia.edu>
2018-01-31 22:04:37 -0500

    Testing output

--- socket-server/main.php
+++ socket-server/main.php
@@ -17,19 +17,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+while(true) {
+  sleep(5);
+  echo "Testing";
+}
 // while(true) {
-    sleep( 5 );
-        $line = fgets($stdin); // I think were going to make this a search function
-
-        $sql = "INSERT INTO messages (sender, type, time, content)
-        VALUES ('John', 'type1', '2017-01-01', 'TESTING')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        echo $line;
+    // sleep( 5 );
+    //     $line = fgets($stdin); // I think were going to make this a search function
+    //
+    //     $sql = "INSERT INTO messages (sender, type, time, content)
+    //     VALUES ('John', 'type1', '2017-01-01', 'TESTING')";
+    //
+    //     if ($conn->query($sql) === TRUE) {
+    //         echo "New record created successfully";
+    //     } else {
+    //         echo "Error: " . $sql . "<br>" . $conn->error;
+    //     }
+    //
+    //     echo $line;
 // }
-$conn->close();
+// $conn->close();
