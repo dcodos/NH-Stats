f61b9a821fd94d575bdb0aacb5377866bd2c4496
Daniel Codos <dbc5ba@virginia.edu>
2018-02-01 20:06:27 -0500

    test send info from miner

--- stats-push-socket/main.py
+++ stats-push-socket/main.py
@@ -2,23 +2,53 @@ import requests
 import json
 from websocket import create_connection
 
-# base_url = 'http://localhost:38080/api?command='
-#
-# command_info = '{"id":1,"method":"device.list","params":[]}'
-# res = requests.get(base_url + command_info)
-#
-# dev_info = json.loads(res.content)["devices"]
-# num_devices = len(dev_info)
-#
-# print(dev_info)
-# print(num_devices)
+base_url = 'http://localhost:38080/api?command='
+
+command_info = '{"id":1,"method":"device.list","params":[]}'
+
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    def run(*args):
+        while True:
+            res = requests.get(base_url + command_info)
+            dev_info = json.loads(res.content)["devices"]
+            num_devices = len(dev_info)
+            ws.send(dev_info)
+            time.sleep(1)
+        ws.close()
+        print("thread terminating...")
+    thread.start_new_thread(run, ())
 
 
-ws = create_connection("ws://ec2-18-216-110-114.us-east-2.compute.amazonaws.com:8888/ws")
-print("Sending 'Hello, World'...")
-ws.send("Hello, World")
-print("Sent")
-print("Receiving...")
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://ec2-18-216-110-114.us-east-2.compute.amazonaws.com:8888/ws",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+# ws = create_connection("ws://ec2-18-216-110-114.us-east-2.compute.amazonaws.com:8888/ws")
+# print("Sending 'Hello, World'...")
+# ws.send("Hello, World")
+# print("Sent")
+# print("Receiving...")
+# result =  ws.recv()
+# print("Received '%s'" % result)
+# ws.close()
